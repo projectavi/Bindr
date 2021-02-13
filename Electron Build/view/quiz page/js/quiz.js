548ef0d0ac1350278a46c13e97d9eec60d790186
Aviraj Newatia <projectavi.exe@gmail.com>
2021-02-13 18:43:57 +0530

    Quiz Page Basic Structure
    Created folder and files for the quiz page structure to be improved upon

---
+++ Electron Build/view/quiz page/js/quiz.js
@@ -0,0 +1,111 @@
+(function(){
+    function buildQuiz(){
+      // variable to store the HTML output
+      const output = [];
+  
+      // for each question...
+      myQuestions.forEach(
+        (currentQuestion, questionNumber) => {
+  
+          // variable to store the list of possible answers
+          const answers = [];
+  
+          // and for each available answer...
+          for(letter in currentQuestion.answers){
+  
+            // ...add an HTML radio button
+            answers.push(
+              `<label>
+                <input type="radio" name="question${questionNumber}" value="${letter}">
+                ${letter} :
+                ${currentQuestion.answers[letter]}
+              </label>`
+            );
+          }
+  
+          // add this question and its answers to the output
+          output.push(
+            `<div class="question"> <b> ${currentQuestion.question} </b> </div> <br>
+            <div class="answers"> ${answers.join('')} </div> `
+          );
+        }
+      );
+  
+      // finally combine our output list into one string of HTML and put it on the page
+      quizContainer.innerHTML = output.join('');
+    }
+  
+    function showResults(){
+  
+      // gather answer containers from our quiz
+      const answerContainers = quizContainer.querySelectorAll('.answers');
+  
+      // keep track of user's answers
+      let numCorrect = 0;
+  
+      // for each question...
+      myQuestions.forEach( (currentQuestion, questionNumber) => {
+  
+        // find selected answer
+        const answerContainer = answerContainers[questionNumber];
+        const selector = `input[name=question${questionNumber}]:checked`;
+        const userAnswer = (answerContainer.querySelector(selector) || {}).value;
+  
+        // if answer is correct
+        if(userAnswer === currentQuestion.correctAnswer){
+          // add to the number of correct answers
+          numCorrect++;
+  
+          // color the answers green
+          answerContainers[questionNumber].style.color = 'lightgreen';
+        }
+        // if answer is wrong or blank
+        else{
+          // color the answers red
+          answerContainers[questionNumber].style.color = 'red';
+        }
+      });
+  
+      // show number of correct answers out of total
+      resultsContainer.innerHTML = `${numCorrect} out of ${myQuestions.length}`;
+    }
+  
+    const quizContainer = document.getElementById('quiz');
+    const resultsContainer = document.getElementById('results');
+    const submitButton = document.getElementById('submit');
+    const myQuestions = [
+      {
+        question: "Question 1",
+        answers: {
+          a: "Answer 1",
+          b: "Answer 2",
+          c: "Answer 3"
+        },
+        correctAnswer: "c"
+      },
+      {
+        question: "Question 2",
+        answers: {
+            a: "Answer 1",
+            b: "Answer 2",
+            c: "Answer 3"
+        },
+        correctAnswer: "c"
+      },
+      {
+        question: "Question 3",
+        answers: {
+            a: "Answer 1",
+            b: "Answer 2",
+            c: "Answer 3"
+        },
+        correctAnswer: "a"
+      }
+    ];
+  
+    // Kick things off
+    buildQuiz();
+  
+    // Event listeners
+    submitButton.addEventListener('click', showResults);
+  })();
