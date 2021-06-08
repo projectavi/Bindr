776fd9f367b99a13f7729741cf43e9fb66198679
Aditya Rao <adityakrao.projects@gmail.com>
2021-06-08 19:18:16 +0530

    Content/Resource System foundation
    Content and resources can be pulled from Firebase (Firebase rules not working implemented right now). Homescreen file created and being worked on currently. Markdown will be implemented to improve content generation in the future.

---
+++ Svelte Build/Bindr/src/homescreen.svelte
@@ -0,0 +1,42 @@
+<script>
+    //export username
+
+    username = "Test"
+
+</script>
+
+<style>
+    .wrapper1 {
+        display:flex;       
+    }
+
+    .banner {
+        height:15%;
+    }
+
+    .option-area {
+        width: 1fr/3;
+    }
+
+</style>
+
+<main>
+    <div id="wrapper1">
+        <div id="banner">
+            Welcome to Bindr {username}
+        </div>
+        <div id="wrapper2">
+            <div id="quiz" class="option-area">
+                
+            </div>
+
+            <div id="notes" class="option-area">
+
+            </div>
+
+            <div id="content" class="option-area">
+
+            </div>
+        </div>
+    </div>
+</main>
