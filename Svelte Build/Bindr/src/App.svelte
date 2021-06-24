785d10e51bb2720f7152721ba4a8c71a3df5d6ab
Aviraj Newatia <projectavi.exe@gmail.com>
2021-06-24 16:53:18 +0530

    Routing Setup
    Basic Routing with a navigation bar in svelte

--- Svelte Build/Bindr/src/App.svelte
+++ Svelte Build/Bindr/src/App.svelte
@@ -6,6 +6,14 @@
 	import Homescreen from './homescreen.svelte';
 	export let name;
 
+	import { Router, Route, Link } from "svelte-navigator";
+	import Login from "./Login.svelte";
+	import PrivateRoute from "./PrivateRoute.svelte";
+	import { user } from "./store";
+
+	function handleLogout() {
+		$user = null;
+	}
 
 	function login() {
 		auth.signInWithPopup(googleProvider);
@@ -18,8 +26,35 @@
 	<button on:click={login}>Login with Google</button>
 	<Content/>
 	-->
+
+	<Router>
+		<header>
+		  <h1>History</h1>
+	  
+		  <nav>
+			<Link to="/">Home</Link>
+			<Link to="about">About</Link>
+			<Link to="profile">Profile</Link>
+		  </nav>
+		</header>
+	  
+		<main>
+		  <Route path="login">
+			<Login />
+		  </Route>
+	  
+		  <Route path="/">
+			<Homescreen/>	
+		  </Route>
 	
-	<Homescreen/>	
+	  
+		  <PrivateRoute path="profile" let:location>
+			<h3>Welcome {$user.username}</h3>
+			<button on:click={handleLogout}>Logout</button>
+		  </PrivateRoute>
+		</main>
+	  </Router>
+	
 </main>
 
 <style>
---
+++ Svelte Build/Bindr/src/login.svelte
@@ -0,0 +1,1 @@
+<h1> Login/Account Page </h1>
